19b9a88e4706dc2494fab498985eb48f50e73f8c
Takuro KAKOI <tkakoi@orphe.io>
2021-09-21 13:43:30 +0900

    migrate example code

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,11 @@
+// ignore_for_file: unnecessary_new
+
 import 'package:flutter/material.dart';
 import 'package:toast/toast.dart';
 
 void main() => runApp(new MyApp());
 
+// ignore: use_key_in_widget_constructors
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -11,13 +14,13 @@ class MyApp extends StatelessWidget {
       theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: new MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, this.title}) : super(key: key);
+  const MyHomePage({Key? key, this.title}) : super(key: key);
   final String? title;
 
   @override
@@ -37,32 +40,37 @@ class _MyAppState extends State<MyHomePage> {
             children: <Widget>[
               Padding(
                 padding: const EdgeInsets.all(10.0),
+                // ignore: deprecated_member_use
                 child: RaisedButton(
-                    child: Text('Show Short Toast'),
+                    child: const Text('Show Short Toast'),
                     onPressed: () => showToast("Show Short Toast")),
               ),
               Padding(
                 padding: const EdgeInsets.all(10.0),
+                // ignore: deprecated_member_use
                 child: RaisedButton(
-                    child: Text('Show Long Toast'),
+                    child: const Text('Show Long Toast'),
                     onPressed: () => showToast("Show Long Toast", duration: Toast.lengthLong)),
               ),
               Padding(
                 padding: const EdgeInsets.all(10.0),
+                // ignore: deprecated_member_use
                 child: RaisedButton(
-                    child: Text('Show Bottom Toast'),
+                    child: const Text('Show Bottom Toast'),
                     onPressed: () => showToast("Show Bottom Toast", gravity: Toast.bottom)),
               ),
               Padding(
                 padding: const EdgeInsets.all(10.0),
+                // ignore: deprecated_member_use
                 child: RaisedButton(
-                    child: Text('Show Center Toast'),
+                    child: const Text('Show Center Toast'),
                     onPressed: () => showToast("Show Center Toast", gravity: Toast.center)),
               ),
               Padding(
                 padding: const EdgeInsets.all(10.0),
+                // ignore: deprecated_member_use
                 child: RaisedButton(
-                    child: Text('Show Top Toast'),
+                    child: const Text('Show Top Toast'),
                     onPressed: () => showToast(
                         """所爱隔山海，山海皆可平。可是你不爱我啊，隔了座火焰山还拿不到芭蕉扇。我奋不顾身穿山越岭到了你身旁，你也只会来一句“卧槽你好666啊”""",
                         gravity: Toast.top)),
